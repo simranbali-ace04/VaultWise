5da85c11025555d8d448d9156fa5f3d4f5246ad5
Simran Bali <simranbali2006@gmail.com>
2026-05-08 20:32:10 +0530

    Initialised the folders

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-[#050505] flex items-center justify-center p-4 md:p-10">
+      <div className="w-full max-w-6xl h-full max-h-212.5 bg-black border border-[#00FF41] rounded-xl overflow-hidden flex flex-col shadow-2xl">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
