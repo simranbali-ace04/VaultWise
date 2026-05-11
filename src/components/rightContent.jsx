7117d159a6f47b5eb30744c8d95cdee6ae5f3043
Simran Bali <simranbali2006@gmail.com>
2026-05-12 00:24:45 +0530

    not desired alignment

--- src/components/rightContent.jsx
+++ src/components/rightContent.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import PasswordCard from './passwordCard';
+import React from "react";
+import PasswordCard from "./passwordCard";
 const RightContent = () => {
   return (
-    <div className='bg-black border border-[#00FF41] md:w-7/12 w-full h-full flex flex-col rounded-xl overflow-hidden p-4 gap-0.5 font-mono'>
-      <div>
-        <h1 className='text-white text-2xl md:text-3xl'>Stored Passwords</h1>
-        <h1 className='text-[#b6b3b3c1] text-xl md:text-2xl'>$ ls --all-passwords</h1>
+    <div className="bg-black border border-[#00FF41] md:w-7/12 w-full h-full flex flex-col rounded-xl overflow-hidden p-4 gap-0.5 font-mono">
+      <div className="shrink-0 mb-4">
+        <h1 className="text-white text-2xl md:text-3xl">Stored Passwords</h1>
+        <h1 className="text-[#b6b3b3c1] text-xl md:text-2xl">
+          $ ls --all-passwords
+        </h1>
       </div>
-      <div>
+      <div className="min-h-0 flex-1 flex flex-wrap content-start justify-start p-2 overflow-y-auto gap-4 custom-scrollbar">
+        <PasswordCard />
+        <PasswordCard />
+        <PasswordCard />
+        <PasswordCard />
+        <PasswordCard />
+        <PasswordCard />
         <PasswordCard />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default RightContent;
