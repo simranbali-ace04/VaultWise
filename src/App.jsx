04ea5584991d6d7faee45061dd12c772be5ce7bb
Simran Bali <simranbali2006@gmail.com>
2026-05-11 15:47:08 +0530

    Add new credentials card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './components/navbar'
 import {Vault} from 'lucide-react'
+import NewCredentials from './components/newCredentials'
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-[#050505] flex items-center justify-center p-4 md:p-10">
+    <div className="min-h-screen bg-[#050505] flex items-center justify-center p-2 md:p-10 ">
       <div className="w-full max-w-6xl h-full max-h-212.5 bg-black border border-[#00FF41] rounded-xl overflow-hidden flex flex-col shadow-2xl">
         <Navbar />
+        <div className='flex flex-col md:flex-row gap-5 p-4 h-full w-full'>
+        <NewCredentials />
+        </div>
       </div>
     </div>
   )
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="font-mono flex flex-col md:flex-row p-2 items-center justify-between px-10 w-full">
       <div className="flex flex-row gap-4 items-center">
-      <Vault className="text-[#b6b3b3c1] h-28 w-24 sm:h-20 sm:w-20" />
+      <Vault className="text-[#b6b3b3c1] md:h-28 md:w-24 h-20 w-20" />
       <div className="flex flex-col p-2">
         <h1 className="text-[#00FF41] text-3xl md:text-6xl tracking-widest">VAULTWISE</h1>
         <h1 className="text-[#b6b3b3c1] mt-1 md:text-xl ">
