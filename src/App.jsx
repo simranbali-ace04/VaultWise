e330fb4c72de14646fdda4f24bcfa7ba53a52ca7
Simran Bali <simranbali2006@gmail.com>
2026-05-12 01:53:57 +0530

    full responsiveness failed

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ const App = () => {
       <div className="w-full max-w-6xl h-[85vh] max-h-212.5 bg-black border border-[#00FF41] rounded-xl overflow-hidden flex flex-col shadow-2xl">
         <Navbar />
         <div className="flex flex-1 min-h-0 flex-col md:flex-row gap-5 p-4 h-full w-full">
-        <LeftContent />
-        <RightContent />
+        <LeftContent/>
+        <RightContent/>
         </div>
       </div>
     </div>
