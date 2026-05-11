98cfb0ca074d635739cd29f2cf93ccdf1645b0e3
Simran Bali <simranbali2006@gmail.com>
2026-05-11 22:37:33 +0530

    Added new credentials in app.jsx

--- src/components/leftContent.jsx
+++ src/components/leftContent.jsx
@@ -1,1 +1,12 @@
+import React from "react";
+import NewCredentials from "./newCredentials";
 
+const LeftContent = () => {
+  return (
+    <div className="flex flex-col md:flex-row gap-5 p-4 h-full w-full">
+      <NewCredentials />
+    </div>
+  );
+};
+
+export default LeftContent;
